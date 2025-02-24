037c1a10cd771c09a8ca8a6b8fcf06c6752c1553
theophilusnuer <tphilus003@gmail.com>
2025-02-24 19:34:27 +0000

    navbar update

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,60 +1,67 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { HashLink } from 'react-router-hash-link';
 import siteIcon from '../asset/image/site icon.png';
 
-
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-
+  const [isScrolled, setIsScrolled] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  // Detect scroll position
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <nav className="fixed w-full bg-white bg-opacity-50 shadow-md p-2 z-50">
+    <nav
+      className={`fixed w-full p-2 z-50 transition-all duration-300 ${
+        isScrolled ? 'bg-white shadow-md' : 'bg-transparent'
+      }`}
+    >
       <div className="container mx-auto flex justify-between items-center">
-        <div className='flex'>
-          <img className='w-12 h-12' src={siteIcon} alt="" />
-          <a href="/" className="text-black text-lg font-bold pt-2 font-custom">K-Links Travel & Tour</a>
+        <div className="flex items-center">
+          <img className="w-12 h-12" src={siteIcon} alt="Site Icon" />
+          <a
+            href="/"
+            className={`text-lg font-bold pt-2 font-custom ${
+              isScrolled ? 'text-black' : 'text-white'
+            }`}
+          >
+            K-Links Travel & Tour
+          </a>
         </div>
-        <div className="hidden md:flex space-x-6 items-center px-2">
+
+        <div className={`hidden md:flex space-x-6 items-center px-2 ${isScrolled ? 'text-black' : 'text-white'}`}>
           <a href="/" className="">Home</a>
-          <HashLink to='/#aboutus'>
-            <a href='' className="">Who We Are</a>
-          </HashLink>
-
-          <HashLink to='/#services'>
-            <a href='' className="">What We Do </a>
-          </HashLink>
-
-          <HashLink to='/#testimonials'>
-            <a href='' className="">Testimonials</a>
-          </HashLink>
-
-          <HashLink to='/#footer'>
-            <a href='' className="">Contact</a>
-          </HashLink>
+          <HashLink to="/#aboutus" className="">Who We Are</HashLink>
+          <HashLink to="/#services" className="">What We Do</HashLink>
+          <HashLink to="/#testimonials" className="">Testimonials</HashLink>
+          <HashLink to="/#footer" className="">Contact</HashLink>
         </div>
 
         <div className="md:hidden flex items-center space-x-4">
-
-          <button className="text-red-600" onClick={toggleMenu}>
+          <button className={`${isScrolled ? 'text-black' : 'text-white'}`} onClick={toggleMenu}>
             {isOpen ? <FaTimes className="w-6 h-6" /> : <FaBars className="w-6 h-6" />}
           </button>
         </div>
       </div>
+
       {isOpen && (
         <div className="md:hidden bg-white mt-2 p-4 space-y-2">
-          <a href="/" className="block  p-2">Home</a>
-          <a href="/#aboutus" className="block  p-2">Who We Are</a>
-
-          <a href='/#services' className="block  p-2">What We Do </a>
-          <a href='/#testimonials' className="block  p-2">Testimonials</a>
-
-          <a href="/#footer" className="block  p-2">Contact</a>
-
+          <a href="/" className="block p-2 text-black">Home</a>
+          <a href="/#aboutus" className="block p-2 text-black">Who We Are</a>
+          <a href="/#services" className="block p-2 text-black">What We Do</a>
+          <a href="/#testimonials" className="block p-2 text-black">Testimonials</a>
+          <a href="/#footer" className="block p-2 text-black">Contact</a>
         </div>
       )}
     </nav>
